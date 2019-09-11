7c7bd70dec7fd16d75ca21bd607e5c5a57f7f31c
Andrew Kelley <andrew@ziglang.org>
2019-09-10 23:39:32 -0400

    change I/O mode with 1 line

--- example/nox.zig
+++ example/nox.zig
@@ -4,21 +4,29 @@ const window = @import("window");
 pub const io_mode = .evented;
 
 pub fn main() !void {
-    const loop = std.event.Loop.instance.?;
-    try loop.initSingleThreaded(std.heap.direct_allocator);
-    defer loop.deinit();
+    if (io_mode == .evented) {
+        const loop = std.event.Loop.instance.?;
+        try loop.initSingleThreaded(std.heap.direct_allocator);
+        defer loop.deinit();
 
-    var result: @typeOf(asyncMain).ReturnType.ErrorSet!void = undefined;
-    var frame: @Frame(asyncMain) = undefined;
-    _ = @asyncCall(&frame, &result, asyncMain, loop);
-    loop.run();
-    return result;
+        var result: @typeOf(asyncMain).ReturnType.ErrorSet!void = undefined;
+        var frame: @Frame(asyncMain) = undefined;
+        _ = @asyncCall(&frame, &result, asyncMain, loop);
+        loop.run();
+        return result;
+    } else {
+        return allMain();
+    }
 }
 
 async fn asyncMain(loop: *std.event.Loop) !void {
     loop.beginOneEvent();
     defer loop.finishOneEvent();
 
+    return allMain();
+}
+
+fn allMain() !void {
     const conn = try window.openDefaultDisplay(std.heap.direct_allocator);
     switch (conn.status) {
         .Ok => {},
